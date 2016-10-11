24417a587608aa537c16732a44342bf7398378ae
hanifekurnaz <hanife.kurnaz@bil.omu.edu.tr>
2016-10-11 18:34:03 +0300

    Update ikilik.py

--- Decimal Sayıyı Binarye Çevirme/ikilik.py
+++ Decimal Sayıyı Binarye Çevirme/ikilik.py
@@ -1,4 +1,4 @@
-dizi=[]
+list=[]
 snc=""
 son=""
 sayi=input("Dönüştürmek istediğiniz sayıyı giriniz")
@@ -6,9 +6,7 @@ while sayi!=0:
     kalan=int(sayi)%2
     sayi=int(sayi)/2
     snc+=str(kalan)
-    dizi.append(snc)
+    list.append(snc)
 for i in snc[::-1]:
     son+=str(i)
 print(son)
-    
-
