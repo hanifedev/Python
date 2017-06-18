d20ef40058d436ec3eb762f9dc7a49e9513997ee
hanifekurnaz <hanife.kurnaz@bil.omu.edu.tr>
2017-06-18 17:50:13 +0300

    Create sistembilgisi.py

---
+++ sistembilgisi.py
@@ -0,0 +1,8 @@
+def sistem_bilgisi_goster():
+  import sys
+  print("\n Sistemde kurulu python'ın")
+  print("\t ana sürüm numarası: ", sys.version_info.major)
+  print("\t alt sürüm numarası: ", sys.version_info.minor)
+  print("\n Kullanılan işletim sisteminin")
+  print("\t adı: " , sys.platform)
+sistem_bilgisi_goster()
