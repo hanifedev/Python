4287e99a2fa4acd4de8f1a77aec8d1fffa36f30d
hanifekurnaz <hanife.kurnaz@bil.omu.edu.tr>
2017-06-28 12:19:20 +0300

    tweepy ile anasayfadaki tweetleri alma

---
+++ timelinelistele.py
@@ -0,0 +1,14 @@
+#! /usr/bin/python
+import tweepy
+consumer_key = ""
+consumer_secret = ""
+access_token = "" 
+access_token_secret = ""
+
+giris = tweepy.OAuthHandler(consumer_key, consumer_secret)
+giris.set_access_token(access_token, access_token_secret)
+api = tweepy.API(giris)
+twitler = api.home_timeline()
+for twit in twitler:
+    print twit.text
+    print "**"*30
